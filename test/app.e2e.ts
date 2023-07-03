2f9f137351b5b58584d69346f3adeed9250d058a
songbinghui <soongbinghui@gmail.com>
2023-07-03 19:19:45 +0800

    feat: vitest e2e

--- test/app.e2e.ts
+++ test/app.e2e.ts
@@ -1,8 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import request from 'supertest';
-import { AppModule } from './../src/app.module';
-import { beforeEach, describe, it } from 'vitest';
+import { AppModule } from '../src/app.module';
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
