3f66c43178b79efdebac43c0b5b086c4951260d9
songbinghui <soongbinghui@gmail.com>
2023-07-05 22:51:55 +0800

    feat: cache

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('Api')
+    .setDescription('The API description')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
+
 bootstrap();
